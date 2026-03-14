9b49656478de9ee5807385d3f548813c8febf0db
Ashvin <ashvinpawar3110@gmail.com>
2026-03-15 00:04:13 +0530

    4

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -16,19 +16,15 @@ import ManageUsers from "../admin/pages/ManageUsers";
 import AdminLayout from "../admin/layout/AdminLayout";
 
 import ProtectedRoute from "../components/ProtectedRoute";
-import Kg1Student from "../admin/pages/Kg1Student";
-import Kg2Student from "../admin/pages/Kg2Student";
-import StudentsAdd from "../admin/pages/StudentsAdd";
 
-import AllTeacher from "../admin/pages/AllTeacher";
-import AddTeacher from "../admin/pages/AddTeacher";
-import AssignClass from "../admin/pages/AssignClass";
-
-import ClassManegement from "../admin/pages/ClassManegement";
 import FeeMangement from "../admin/pages/FeeMangement";
 import Announcement from "../admin/pages/Announcement";
 import GalleryAdmin from "../admin/pages/GalleryAdmin";
 import { Settings } from "lucide-react";
+
+import StudentManagement from "../admin/pages/StudentManagement";
+import TeacherManagement from "../admin/pages/TeacherManagement";
+import ClassManagement from "../admin/pages/ClassManagement";
 
 
 function AppRoutes() {
@@ -59,29 +55,19 @@ function AppRoutes() {
           <Route index element={<AdminDashboard />} />
           <Route path="dashboard" element={<AdminDashboard />} />
 
-          <Route path="students/all" element={<ManageUsers />} />
+          <Route path="student" element={<StudentManagement />} />
 
-          <Route path="students/kg1" element={<Kg1Student />} />
+          <Route path="teacher" element={<TeacherManagement />} />
 
-          <Route path="students/kg2" element={<Kg2Student />} />
-
-          <Route path="students/add" element={<StudentsAdd />} />
-
-          <Route path="teachers/all" element={<AllTeacher />} />
-
-          <Route path="teachers/add" element={<AddTeacher />} />
-
-          <Route path="teachers/assign" element={<AssignClass />} />
-
-          <Route path="classes" element={<ClassManegement />} />
+          <Route path="class" element={<ClassManagement />} />
 
           <Route path="fees" element={<FeeMangement />} />
 
           <Route path="announcements" element={<Announcement />} />
 
-           <Route path="gallery" element={<GalleryAdmin />} />
+          <Route path="gallery" element={<GalleryAdmin />} />
 
-            <Route path="settings" element={<Settings />} />
+          <Route path="settings" element={<Settings />} />
 
         </Route>
 
